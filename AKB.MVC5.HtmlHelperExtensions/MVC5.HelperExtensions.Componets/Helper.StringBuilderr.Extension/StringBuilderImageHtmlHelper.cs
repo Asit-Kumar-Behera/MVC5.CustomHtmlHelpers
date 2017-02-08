127e2dbcab6a4428aa5ab2f9204d1497b330ef3b
asit-kumar-behera <asitbehera2005@gmail.com>
2017-02-08 06:06:35 +0530

    Removed the capacity parameter of the StringBuilder constructor

--- AKB.MVC5.HtmlHelperExtensions/MVC5.HelperExtensions.Componets/Helper.StringBuilderr.Extension/StringBuilderImageHtmlHelper.cs
+++ AKB.MVC5.HtmlHelperExtensions/MVC5.HelperExtensions.Componets/Helper.StringBuilderr.Extension/StringBuilderImageHtmlHelper.cs
@@ -19,7 +19,7 @@ namespace AKB.MVC5.HtmlHelperExtensions.MVC5.HelperExtensions.Componets.Helper.S
     /// <returns></returns>
         public static MvcHtmlString Image(this HtmlHelper htmlHelper,string src,string altText)
         {
-            StringBuilder sb = new StringBuilder(512);
+            StringBuilder sb = new StringBuilder();
             sb.AppendFormat("<img src='{0}' alt='{1}'/>",src,altText);
 
             return MvcHtmlString.Create(sb.ToString());
